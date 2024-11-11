f835d61636b512a14cf21f0f3ea486ebcc941b22
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-11-10 22:00:58 -0600

    Committed by Sakxu

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import History from "./components/History";
+import Signup from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />{" "}
       <Route path="/history" element={<History />} />{" "}
+      <Route path="/signup" element={<Signup />} /> 
+      <Route path="/login" element={<Login />} /> 
+      
     </Routes>
   );
 }
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,18 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCSaV172eht5nwdkvw3oMYU2bnLFcKp2lY",
+    authDomain: "moody-f079c.firebaseapp.com",
+    projectId: "moody-f079c",
+    storageBucket: "moody-f079c.firebasestorage.app",
+    messagingSenderId: "981886508143",
+    appId: "1:981886508143:web:92012b2600f50cb47bc19a",
+    measurementId: "G-TV7WYTP571"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
